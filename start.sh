830dc41ce694ea95294086e7c2c2b62d92e225ad
teamsuraj2 <surajsingh80092002@gmail.com>
2026-02-14 18:41:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,9 +5,9 @@ then
   git clone https://github.com/teamsuraj2/F teamsuraj2/F
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/teamsuraj2/F -b $BRANCH /Ultra-Forward-Bot
+  git clone https://github.com/teamsuraj2/F -b $BRANCH /teamsuraj2/F
 fi
-cd JishuDeveloper/Ultra-Forward-Bot
+cd teamsuraj2/F
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
